6673262379e474cda140803e9e03c4c12b59b2ba
Arturo Filastò <arturo@filasto.net>
2018-06-02 17:48:07 +0200

    Fix version injection

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "github.com/ooni/collector/cmd"
+import (
+	"github.com/ooni/collector/cmd"
+	"github.com/ooni/collector/collector/info"
+)
+
+var (
+	version = "dev"
+	commit  = ""
+	date    = "unknown"
+)
 
 func main() {
+	info.Version = version
+	info.CommitHash = commit
+	info.BuildDate = date
 	cmd.Execute()
 }
